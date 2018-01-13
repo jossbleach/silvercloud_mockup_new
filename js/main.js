b3a4798c43f1e2bc8433849261d8369dbd3e5152
jossbleach <jossjbleach@gmail.com>
2018-01-13 21:08:54 +0000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 
+    $(function initMap() {
+      var myLatLng = {lat: 51.53066, lng: -0.093566};
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: myLatLng
+      });
+      var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Hello World!'
+      });
+    })
     $(function() {
        //Scrolling Effects
        var navLogo = $(".navlogo");
